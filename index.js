4ca632f2b45c15e8ba5fb3622c37e30d0118c7f1
Ziad Alfadl <alfadlziad@gmail.com>
2023-07-19 18:34:54 -0400

    edit questions list/generateMarkdown function

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer')
-const generateMarkdown = require('./utils/generateMarkdown.js')
-// const generateMarkdown = require('generateMarkdown')
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const { default: Choices } = require('inquirer/lib/objects/choices.js');
+
+const licenseOptions = [
+    'No license used',
+    'GNU AGPLv3',
+    'GNU GPLv3',
+    'GNU LGPLv3',
+    'Mozilla Public License 2.0',
+    'Apache License 2.0',
+    'MIT License',
+    'Boost Software License 1.0',
+    'The UnLicense',
+    ]
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -16,6 +28,11 @@ const questions = [
         message: 'Please describe what your project does.'
     },
     {
+        type: 'confirm',
+        name: 'tableofcontents',
+        message: 'Would you like to include a table of contents?'
+    },
+    {
         type: 'input',
         name: 'installation',
         message: 'How can a user install your project on their own device?'
@@ -24,17 +41,6 @@ const questions = [
         type: 'input',
         name: 'usage',
         message: 'Please explain how your project is meant to be used'
-    },
-    {
-        type: 'confirm',
-        name: 'confirmation',
-        message: 'Can others contribute to this project?'
-    },
-    {
-        type: 'input',
-        name: 'contribution',
-        message: 'How can someone contribute to this project?',
-        when: (answer) => answer.confirmation === true
     },
     {
         type: 'confirm',
@@ -48,27 +54,35 @@ const questions = [
         when: (answer) => answer.confirmCredit === true
     },
     {
+        type: 'confirm',
+        name: 'confirmation',
+        message: 'Can others contribute to this project?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How can someone contribute to this project?',
+        when: (answer) => answer.confirmation === true
+    },
+    {
         type: 'input',
         name: 'tests',
         message: 'How do test?!'
     },
     {
-        type: 'confirm',
-        name: 'confirmLicense',
-        message: 'Did you use any licenses?'
-    },
-    {
-        type: 'input',
+        type: 'checkbox',
         name: 'license',
-        message: 'What license are you using',
-        when: (answer) => answer.confirmLicense === true
-    }
+        message: 'Did you use any licenses?',
+        choices: licenseOptions
+    },
+
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(`${fileName}-README.md`, generateMarkdown(data), (err) => {
         err ? console.error(err) : console.log(`${fileName}-README created`)
+        
     })
 }
 
@@ -77,8 +91,27 @@ function init() {
     inquirer.prompt(questions)
     .then((answers) => {
         writeToFile(answers.title, answers)
-    })
+        console.log(answers.license)
+    }
+    )
 }
 
 // Function call to initialize app
 init();
+
+/*
+1. Title of project (name of repository)
+2. Enter description of project.
+3. (confirm) Table of contents to be added.
+4. Enter installation instructions.
+5. Enter usage instructions:
+    a. (confirm) Image to be included? (relative file path?)
+6. Contributions section:
+    a. (confirm) List collaborators.
+    b. (confirm) Third party attributions.
+    c. (confirm) How to contribute
+7. Licenses section:
+    a. (confirm) (checkbox?) Licenses Used 
+    b. (confirm)(checkbox) badges (toplang/license/repo)
+8. (confirm) How to run tests if there are any.
+ */
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -5,7 +5,8 @@ function renderLicenseBadge(license) {
     return
   } else {
     //return license badge
-    return `![License](https://img.shields.io/badge/License-${license})`
+    // console.log(`![License](https://img.shields.io/badge/License-${license})`)
+    return `![${license}](https://img.shields.io/badge/License-${license}.svg)`
   }
 }
 
@@ -16,6 +17,7 @@ function renderLicenseLink(license) {
     return
   } else {
     //return license link
+    // console.log(`[${license}](http://choosealicense.com/licenses/${license})`)
     return `[${license}](http://choosealicense.com/licenses/${license})`
   }
 }
@@ -23,54 +25,112 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
+  
   if (!license) {
     return
   } else {
-    renderLicenseBadge(license)
-    renderLicenseLink(license)
+    var licenseSection =`
+## Licenses`
+    for (let i = 0; i < license.length; i++) {
+      var licensebadges =`
+${renderLicenseBadge(license[i])}`
+
+      var licenseLink = `
+${renderLicenseLink(license[i])}`
+  licenseSection+=licensebadges     
+  licenseSection+=licenseLink      
+    }
+    return licenseSection
+    // renderLicenseBadge(license)
+    // console.log(renderLicenseBadge(license))
+    // console.log(renderLicenseLink(license))
+    // renderLicenseLink(license)
   }
 }
-
+// renderLicenseSection('mit')
+//Test object => 
+// var testObject = {
+//   title: "project",
+//   description: "A string of letters and numb3rs",
+//   tableofcontents: true,
+//   installation: "Use a toolbox",
+//   usage: "You know how to whistle dont ya",
+//   contribution: "Contact through email",
+//   credits: "Thanks Obama",
+//   tests: "Run through node",
+//   license: "MIT"
+// }
 // TODO: Create a function to generate markdown for README
-function generateMarkdown({title, description, installation, usage, contribution, credits, tests, license}
-  ) {
+function generateMarkdown({ 
+  title,
+  description, 
+  tableofcontents, 
+  installation, 
+  usage, 
+  contribution, 
+  credits, 
+  tests, 
+  license }
+) {
   var markdown = `# ${title}
 ## Description
-${description}
-## Table of Contents
-- [Installation](#installation)
-- [Usage](#Usage)
-- [Contribution](#contribution)
-- [Credits](#Credits)
-- [Tests](#tests)
-- [](#)
+${description}`
+
+  if (tableofcontents) {
+    var toc = `
+## Table of Contents`
+    installation ? toc += `
+- [Installation](#installation)` : null
+    usage ? toc += `
+- [Usage](#Usage)` : null
+    contribution ? toc += `
+- [Contribution](#contribution)` : null
+    credits ? toc += `
+- [Credits](#Credits)` : null
+    tests ? toc += `
+- [Tests](#tests)` : null
+    license ? toc += `
+- [License](#license)` : null
+    markdown += toc
+  }
+  installation ? markdown += `
 ## Installation
 ${installation}.
-## Usage
-  ${usage}
-  `;
+` : null
+  if (usage) {
+    var use = `## Usage
+${usage}`
+    markdown += use
+  };
   if (contribution) {
-    var contribute= `## How to Contribute
-    ${contribution}`
+    var contribute =
+      `
+## How to Contribute
+${contribution}
+`
     markdown += contribute
   }
   if (credits) {
-    var credit = 
-    `## With special thanks to:
-  ${credits}`
-  markdown += credit
+    var credit =
+      `## With special thanks to:
+${credits}
+`
+    markdown += credit
   }
   if (tests) {
-    var test = `## Tests
-  ${tests}`
-  markdown += test
+    var test =
+      `## Tests
+${tests}
+`
+    markdown += test
   }
   if (license) {
-   var licenses =`## Licenses
-${renderLicenseSection(license)}`
-    markdown += licenses
+    markdown += renderLicenseSection(license)
   }
+  console.log(markdown)
   return markdown
 }
 
+
+// generateMarkdown(testObject)
 module.exports = generateMarkdown;
